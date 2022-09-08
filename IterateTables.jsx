99b7e2346c44ac96c4dacc32541a733a71cca255
MananJoshi <manan.joshi227@gmail.com>
2022-09-08 09:57:24 +0530

    Script to iterate all the tables in the document

---
+++ IterateTables.jsx
@@ -0,0 +1,21 @@
+/*
+https://community.adobe.com/t5/indesign-discussions/accepting-track-changes-in-tables/td-p/13181717
+
+Script to get an array of all the talbes present in a document
+Pre Requisite:- The script needs an open document
+Warning :- No elaborate error checking
+
+Author:- Manan Joshi
+Guthub Repo :- https://github.com/Manan-Joshi/InDesign-Scripts.git
+*/
+function getTables(pCol){
+    var retval = [];
+    var tables = pCol.everyItem().tables.everyItem().getElements()
+    retval = retval.concat(tables)
+    for(var i = 0; i < tables.length; i++){
+        retval = retval.concat(getTables(tables[i].cells))
+    }
+    return retval
+}
+
+var tables = getTables(app.documents[0].stories)
